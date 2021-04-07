19b4e6a0e9df867375f8c9c5f2bfcc0b9062d17e
chasjohnson09 <78383485+chasjohnson09@users.noreply.github.com>
2021-04-07 09:13:43 -0400

    pushing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({                  // this is a decorator 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hello-world-angular';
+
+import class AppComponent {
+  n: number = 0;
+  m: number = 0;
+  ans: number = 0;
+
+  add(): void {
+    this.ans = this.n + this.m;
+  }
 }
+
+
+
+// export class AppComponent {
+//   title = 'hello-world-angular from Chas a future programmer';
+//   name: string = "Chas Johnson";
+//   mytext: number = "#"
+
+//   changeName(): void {
+//     this.name = "Charles";
+//   } 
+//   add(): void {
+//     "#vara" + "#varb";
+//   }
+// }
